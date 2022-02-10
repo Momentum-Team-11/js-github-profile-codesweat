e7ee905a38f748d8b19afe1bd2e5ddc758817a3b
KarenElaine <karenelaine.stewart@yahoo.com>
2022-02-09 19:58:21 -0500

    completed dev page

--- main.js
+++ main.js
@@ -9,11 +9,23 @@ fetch('https://api.github.com/users/codesweat')
         const root = document.getElementById('root');      
         console.log(root);
         console.log(root.innerHTML)
+        // container div starts line 14
         root.innerHTML = `
-            <div id="content" class="content">
-                <p>GitHub Username: ${data.login}</p>
-                <img src=${data.avatar_url} width="300"/>
-                <h3>${data.name}</h3>
+            <div id="content" class="content is-flex is-align-content-space-between is-flex-direction-column p*">
+                <section id="laguid-lavender" class="box is-flex section is-justify-content-space-between is-align-items-center"> 
+                <figure class="image is-128x128">
+                    <img class="is-rounded" src=${data.avatar_url} width="300"/>
+                    </figure>
+                    <h3>${data.name}</h3> 
+                </section>
+                <section class="box is-flex is-align-items-center section is-justify-content-space-between">
+                    <div class="is-flex is-flex-direction-column">
+                        <p>Location: ${data.location}</p>
+                        <p>GitHub Username: ${data.login}</p>
+                        <a href=${data.html_url} target="_blank">GitHub URL: ${data.html_url}</a>
+                    </div>
+                    <p class="is-flex is-align-items-center">${data.bio}</p>
+                </section>
             </div>
         `
        
@@ -21,19 +33,24 @@ fetch('https://api.github.com/users/codesweat')
         // username.innerText = data.login
         // root.appendChild(username)
     })
-
+        
      // fetch request to get my repos
      fetch('https://api.github.com/users/codesweat/repos')
      .then((response) => response.json())
      .then((repos) => {
+         content.innerHTML += `
+            <section id="repos" class="box section is-flex is-flex-direction-column">
+                <h4>Github Repos</h4> 
+            </section>
+         `
          for (let repo of repos) {
              console.log("repo", repo)
              console.log("repos", repos)
-             let contentDiv = document.getElementById("content")
-             contentDiv.innerHTML += `
-                 <div class="repo">
+             let repoDiv = document.getElementById("repos")
+             repoDiv.innerHTML += ` 
+             <a href=${repo.html_url} target="_blank">
                      ${repo.name}
-                 <div/>
+                 </a>
              `
          }
      })
